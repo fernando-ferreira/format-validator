516c8655e6ff25260140542ef745b7940fa90864
Fernando Ferreira <fernando.ferreira@morpho.com>
2018-06-08 11:53:21 -0300

    Add patterns values for enum

--- src/br/com/sample/util/DataFormatType.java
+++ src/br/com/sample/util/DataFormatType.java
@@ -1,5 +1,17 @@
 package br.com.sample.util;
 
 public enum DataFormatType {
-	CPF, RG, PHONE_NUMBER
+	CPF("[0-9]{3}'.'[0-9]{3}'.'[0-9]{3}'-'[0-9]{2}"), 
+	RG("[0-9]{2}'.'[0-9]{3}'.'[0-9]{3}'-'[0-9]"), 
+	PHONE_NUMBER("[0-9]{4}'-'[0-9]{4}");
+	
+	private String pattern;
+	
+	private DataFormatType(String pattern) {
+		this.pattern = pattern;
+	}
+	
+	public String getDataPattern() {
+		return this.pattern;
+	}
 }
